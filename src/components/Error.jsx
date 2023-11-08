010c2302014e83d3fc33f61a4abc52a6692d79e7
Viana Ferguson <vianaferguson@yahoo.com>
2023-11-07 22:18:05 -0700

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className="mt-2 font-bold text-2xl text-white">
+      Something went wrong. Please try again.
+    </h1>
+  </div>
+);
+
+export default Error;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '1d9a8badf5msh7e66543a67bc4a2p19d8adjsn0972c59dfd81',
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;

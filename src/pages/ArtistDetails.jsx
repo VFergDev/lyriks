9093ae888a4d797cdc32e63093ac5b40ad78189a
Viana Ferguson <vianaferguson@yahoo.com>
2023-11-09 01:59:59 -0700

    final

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,29 +1,26 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { useGetArtistDetailsQuery } from '../redux/services/shazamArtistCore';
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const {
-    data: artistData,
-    isFetching: isFetchingArtistDetails,
-    error,
-  } = useGetArtistDetailsQuery(artistId);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-  if (isFetchingArtistDetails) { return <Loader title="Searching for artist details" />; }
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
   if (error) return <Error />;
 
-  if (!artistData || !artistData.songs) {
-    return console.log(error);
-  }
-
   return (
     <div className="flex flex-col">
-      <DetailsHeader artistId={artistId} artistData={artistData?.data[0]} />
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData?.data[0]}
+      />
+
       <RelatedSongs
         data={artistData?.data[0].views['top-songs']?.data}
         artistId={artistId}
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,32 +1,33 @@
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set(
-        'X-RapidAPI-Key',
-        '1d9a8badf5msh7e66543a67bc4a2p19d8adjsn0972c59dfd81',
-      );
+      headers.set('X-RapidAPI-Key', '1d9a8badf5msh7e66543a67bc4a2p19d8adjsn0972c59dfd81');
+
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
-    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-    getSongsBySearch: builder.query({ query: (SearchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${SearchTerm}` }),
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
   useGetSongsByGenreQuery,
-  useGetSongDetailsQuery,
-  useGetSongRelatedQuery,
   useGetSongsByCountryQuery,
   useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;

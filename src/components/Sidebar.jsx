5ceabaa9fb514fc03118f601581d634931e98c20
Viana Ferguson <vianaferguson@yahoo.com>
2023-11-09 14:48:35 -0700

    close Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,11 +23,15 @@ const NavLinks = ({ handleClick }) => (
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+  const closeSidebar = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624] ">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks />
+        <NavLinks handleClick={closeSidebar} />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
@@ -36,7 +40,7 @@ const Sidebar = () => {
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks />
+        <NavLinks handleClick={closeSidebar} />
       </div>
     </>
   );

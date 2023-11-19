9d1480289170033425fbc4035b3d96d8150ec9fe
Viana Ferguson <vianaferguson@yahoo.com>
2023-11-19 11:37:28 -0700

    reload issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lyriks/",
+  base: '/lyriks/',
   plugins: [react()],
 });

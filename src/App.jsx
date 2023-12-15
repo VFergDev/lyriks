56d9a2a23af9b52574f18b5adb3c735fd9d805b1
Viana Ferguson <vianaferguson@yahoo.com>
2023-12-14 17:11:51 -0700

    move topPlay

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ const App = () => {
           <Searchbar />
 
           <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+            <div className="xl:sticky relative top-0 h-fit">
+              <TopPlay />
+            </div>
             <div className="flex-1 h-fit pb-40">
               <Routes>
                 <Route path="/" element={<Discover />} />
@@ -27,9 +30,7 @@ const App = () => {
                 <Route path="/search/:searchTerm" element={<Search />} />
               </Routes>
             </div>
-            <div className="xl:sticky relative top-0 h-fit">
-              <TopPlay />
-            </div>
+
           </div>
         </div>
 
